7d8f5d1ad362445e50efd30dc0c2e52b61842bbf
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-28 21:19:41 +0900

    Add MatchingEffect
    오리 매칭시 해당 위치에 파티클 이펙트가 생성 및 일정시간 이후 자동 삭제되도록 구현

---
+++ Assets/Scripts/GamePlay/EffectRemove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectRemove : MonoBehaviour
+{
+    private float lifeTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+  
+}
--- Assets/Scripts/Global/EffectManager.cs
+++ Assets/Scripts/Global/EffectManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class EffectManager : MonoBehaviour
 {
     public static EffectManager Instance { get; private set; }
+    public GameObject[] particleEffects;
     public AudioClip[] soundEffects;
     private AudioSource audioSource;
+    private Transform effectTransform;
 
     void Awake()
     {
@@ -39,5 +41,10 @@ public class EffectManager : MonoBehaviour
         audioSource.PlayOneShot(soundEffects[1]);
     }
 
+    public void MatchEffect(int gridNum)
+    {
+        effectTransform = GridManager.Instance.Ducks[gridNum].transform;
+        Instantiate(particleEffects[0], effectTransform.position, effectTransform.rotation);
+    }
 
 }
