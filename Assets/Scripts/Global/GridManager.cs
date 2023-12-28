f506342053cc808ea10e516474bc8574adfe51ee
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-12-28 14:48:11 +0900

    FindObjectLogic
    화면 클릭시 마우스 위치를 기반으로 계산하여 해당 위치에 있는 오브젝트중 몇 번째 오리의 activeself가 트루로 되어있는지 판단하는 로직

--- Assets/Scripts/Global/GridManager.cs
+++ Assets/Scripts/Global/GridManager.cs
@@ -31,9 +31,9 @@ public class GridManager : MonoBehaviour
 
         for (int i = 0; i < gridNum; i++)
         {
-                HideDuck(i,0);
-                gridArray[i] = Random.Range(0, 5);
-                ShowDuck(i,gridArray[i]);
+            HideDuck(i, 0);
+            gridArray[i] = Random.Range(0, 5);
+            ShowDuck(i, gridArray[i]);
         }
     }
 
@@ -47,11 +47,6 @@ public class GridManager : MonoBehaviour
     private void HideDuck(int gridNum, int duckNum)
     {
         Ducks[gridNum].transform.GetChild(duckNum).gameObject.SetActive(false);
-    }
-
-    private void ChangeDuck(int input, int output)
-    {
-        
     }
 
 }
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -6,15 +6,29 @@ public class PlayerAction : MonoBehaviour
 {
     Vector2 mousePos = Vector2.zero;
     public Camera Camera;
+    public GameObject MyDuck;
+
+    private float row = 0;
+    private float col = 0;
+    private int clickPos;
+    private int gridDuckNum;
+    private int MyDuckNum;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GetMousePos();
-            Debug.Log(mousePos);
+            PlayerClick();
         }
     }
+
+    private void PlayerClick()
+    {
+        GetMousePos();
+        FindClickObject();
+        ChangeDuck();
+    }
+
 
     private void GetMousePos()
     {
@@ -22,6 +36,48 @@ public class PlayerAction : MonoBehaviour
         mousePos = Camera.ScreenToWorldPoint(mousePos);
 
     }
+
+    private void FindClickObject()
+    {
+        mousePos.x = mousePos.x * 100 + 245;
+        mousePos.y = mousePos.y * 100 + 340;
+        col = mousePos.x / 70;
+        row = mousePos.y / 70;
+
+        clickPos = 49 - ((int)row*7 - (int)col) ;
+
+        Debug.Log(row);
+        Debug.Log(col);
+        Debug.Log(clickPos);
+
+        gridDuckNum = GridManager.Instance.gridArray[clickPos];
+        Debug.Log(gridDuckNum);
+        CheckMyDuck();
+        Debug.Log(MyDuckNum);
+
+    }
+
+    private void CheckMyDuck()
+    {
+        for(int i=0; i<5; i++)
+        {
+            if (MyDuck.transform.GetChild(i).gameObject.activeSelf == true)
+            {
+                MyDuckNum = i;
+                break;
+            }
+               
+        }
+      
+    }
+
+    private void ChangeDuck()
+    {
+      
+
+    }
+
+
 
 
 }
